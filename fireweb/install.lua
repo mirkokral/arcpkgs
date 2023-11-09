7dddf804ac8e81d7043d8423e760108bad4b2ed1
mirkokral <75212488+mirkokral@users.noreply.github.com>
2023-11-09 17:07:55 +0100

    forgot a double v

--- fireweb/install.lua
+++ fireweb/install.lua
@@ -1,1 +1,1 @@
-local h=http.get("https://raw.githubusercontent.com/emir4169/FireWeb/master/Main.lua");local f=fs.open("packages/fireweb/bin/fw.lua");f.write(h.readAll());f.close();h.close()
+local h=http.get("https://raw.githubusercontent.com/emir4169/FireWeb/master/Main.lua");local f=fs.open("packages/fireweb/bin/fw.lua", "w");f.write(h.readAll());f.close();h.close()
