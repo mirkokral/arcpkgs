bda63b6b56cb5c8e63d4768ac716394cc217d4ad
mirkokral <75212488+mirkokral@users.noreply.github.com>
2023-11-10 14:20:51 +0100

    Disable fireweb updater on install because it breaks non-fullPriv support.

--- fireweb/install.lua
+++ fireweb/install.lua
@@ -1,1 +1,3 @@
 local h=http.get("https://raw.githubusercontent.com/emir4169/FireWeb/master/Main.lua");local f=fs.open("packages/fireweb/bin/fw.lua", "w");f.write(h.readAll());f.close();h.close()
+settings.set("fireweb.update", false)
+settings.save()
